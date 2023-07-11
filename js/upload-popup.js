3a1838d94503bef30fc7f01410c6a602b8edba3e
Aleks K <kadmon669@gmail.com>
2023-07-11 18:05:53 +0300

    module9-task2

--- js/upload-popup.js
+++ js/upload-popup.js
@@ -7,6 +7,7 @@ const popup = document.querySelector('.img-upload__overlay');
 const preview = popup.querySelector('img');
 const scaleControl = initScaleControl(popup.querySelector('.scale'));
 const effectSlider = initEffectSlider(popup.querySelector('.effect-level'));
+const effectPicker = popup.querySelector('.effects');
 
 /**
  * @param {File} data
@@ -19,7 +20,9 @@ function renderPopup(data) {
   scaleControl.setValue(100);
 
   effectSlider.on('update', onEffectSliderUpdate);
-  effectSlider.setEffect('phobos');
+  effectSlider.setEffect('none');
+
+  effectPicker.addEventListener('change', onEffectPickerChange);
 
   showPopup(popup);
 }
@@ -33,6 +36,10 @@ function onScaleControlUpdate() {
 
 function onEffectSliderUpdate() {
   preview.style.setProperty('filter', effectSlider.getCssValue());
+}
+
+function onEffectPickerChange(event) {
+  effectSlider.setEffect(event.target.getAttribute('value'));
 }
 
 export default renderPopup;
--- js/upload.js
+++ js/upload.js
@@ -27,8 +27,6 @@ function onFormChange(event) {
   }
 }
 
-  renderPopup();
-
 function onFormHide() {
   form.reset();
 }
