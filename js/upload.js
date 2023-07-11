dd205df60ad6e19e550ff8412825f3cb33755fda
Aleks K <kadmon669@gmail.com>
2023-07-11 14:05:07 +0300

    add color

--- js/upload.js
+++ js/upload.js
@@ -1,12 +1,20 @@
 import renderPopup from './upload-popup.js';
+import './pristine-validators.js';
 
 /**
  * @type {HTMLFormElement}
  */
 const form = document.querySelector('.img-upload__form');
 
+// @ts-ignore
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper'
+});
+
 form.addEventListener('change', onFormChange);
 form.addEventListener('hide', onFormHide, true);
+form.addEventListener('reset', onFormReset);
 
 /**
  * @param {Event & {target: HTMLInputElement}} event
@@ -21,4 +29,8 @@ function onFormChange(event) {
 
 function onFormHide() {
   form.reset();
+}
+
+function onFormReset() {
+  pristine.reset();
 }
